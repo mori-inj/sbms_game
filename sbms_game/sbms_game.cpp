1edd3821e3dd3e3d448eb23a0ba00031a0e47437
mori-inj <leokim1022@snu.ac.kr>
2016-01-07 05:41:49 +0900

    title, select done

--- sbms_game/sbms_game.cpp
+++ sbms_game/sbms_game.cpp
@@ -1,13 +1,21 @@
 #include <windows.h>
 #include <gdiplus.h>
+#include "resource.h"
 using namespace Gdiplus;
 #pragma comment(lib, "gdiplus")
 
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
 HINSTANCE g_hInst;
 HWND hWndMain;
 LPCTSTR lpszClass = TEXT("GdiPlusStart");
+
+int screen_mode,choice;
+enum screen {title, choose, tutorial, start_anim, ingame, gameover};
+
+void OnPaint(HDC hdc, int ID, int x, int y);
+void OnPaintA(HDC hdc, int ID, int x, int y, double alpha);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
@@ -42,10 +50,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 		L"Window Class Name",
 		L"Window Title Name",
 		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
+		GetSystemMetrics(SM_CXFULLSCREEN)/2-408,
+		GetSystemMetrics(SM_CYFULLSCREEN)/2-319,
+		816,
+		638,
 		NULL,
 		NULL,
 		hInstance,
@@ -63,10 +71,137 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
+	HDC hdc, MemDC;
+	PAINTSTRUCT ps;
+	
+	HBITMAP hBit, OldBit;
+	RECT crt;
+
+
 	switch (iMsg)
 	{
 	case WM_CREATE:
+		screen_mode = screen::title;
+		choice = 0;
+		SetTimer(hWnd, 1, 10, 0);
 		break;
+
+	case WM_TIMER:
+		InvalidateRect(hWnd, NULL, FALSE);
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		GetClientRect(hWnd, &crt);
+
+		MemDC = CreateCompatibleDC(hdc);
+		hBit = CreateCompatibleBitmap(hdc, crt.right, crt.bottom);
+		OldBit = (HBITMAP)SelectObject(MemDC, hBit);	
+		SetBkColor(MemDC, RGB(255, 255, 255));
+
+		switch (screen_mode)
+		{
+		case screen::title:
+			if (choice==0)
+				OnPaint(MemDC, TITLE0, 0, 0);
+			else
+				OnPaint(MemDC, TITLE1, 0, 0);
+			break;
+		case screen::tutorial:
+			OnPaintA(MemDC, TITLE1, 0, 0, 0.7);
+			break;
+		case screen::choose:
+			if(choice==0)
+				OnPaint(MemDC, SELECT_KUDA, 0, 0);
+			else
+				OnPaint(MemDC, SELECT_NOBO, 0, 0);
+			break;
+
+		}
+
+
+		BitBlt(hdc, 0, 0, crt.right, crt.bottom, MemDC, 0, 0, SRCCOPY);
+		SelectObject(MemDC, OldBit);
+		DeleteDC(MemDC);
+		DeleteObject(hBit);
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_UP:
+			switch (screen_mode)
+			{
+			case screen::title:
+				if (choice == 1)
+					choice = 0;
+				break;
+			}
+			break;
+		case VK_DOWN:
+			switch (screen_mode)
+			{
+			case screen::title:
+				if (choice == 0)
+					choice = 1;
+				break;
+			}
+			break;
+		case VK_LEFT:
+			switch (screen_mode)
+			{
+			case screen::choose:
+				if (choice == 1)
+					choice = 0;
+				break;
+			}
+			break;
+		case VK_RIGHT:
+			switch (screen_mode)
+			{
+			case screen::choose:
+				if (choice == 0)
+					choice = 1;
+				break;
+			}
+			break;
+		case VK_RETURN:
+			switch (screen_mode)
+			{
+			case screen::title:
+				if (choice == 0)
+					screen_mode = screen::choose;
+				else
+					screen_mode = screen::tutorial;
+				break;
+			}
+			break;
+		case 'Z':
+			switch (screen_mode)
+			{
+			case screen::title:
+				if (choice == 0)
+					screen_mode = screen::choose;
+				else
+					screen_mode = screen::tutorial;
+				break;
+			}
+			break;
+		case 'X':
+			switch (screen_mode)
+			{
+			case screen::tutorial:
+				choice = 1;
+				screen_mode = screen::title;
+				break;
+			case screen::choose:
+				choice = 0;
+				screen_mode = screen::title;
+				break;
+			}
+		}
+		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
