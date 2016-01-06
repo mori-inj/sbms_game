8cd94c7a7bf98d9472a01d3d04d7264ac097798c
mori-inj <leokim1022@snu.ac.kr>
2016-01-07 04:22:37 +0900

    OnPaint added

--- sbms_game/sbms_game.cpp
+++ sbms_game/sbms_game.cpp
@@ -1,11 +1,32 @@
 #include <windows.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
+#pragma comment(lib, "gdiplus")
+
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("GdiPlusStart");
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
 	HWND     hWnd;
 	MSG		 msg;
 	WNDCLASS WndClass;
+
+	g_hInst = hInstance;
+
+	ULONG_PTR gpToken;
+	GdiplusStartupInput gpsi;
+	if (GdiplusStartup(&gpToken, &gpsi, NULL) != Ok) 
+	{
+		MessageBox(NULL, TEXT("GDI+ 라이브러리를 초기화할 수 없습니다."),TEXT("알림"), MB_OK);
+		return 0;
+	}
+
+
 	WndClass.style = CS_HREDRAW | CS_VREDRAW;
 	WndClass.lpfnWndProc = WndProc;
 	WndClass.cbClsExtra = 0;
@@ -51,4 +72,70 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return DefWindowProc(hWnd, iMsg, wParam, lParam);
+}
+
+void OnPaint(HDC hdc, int ID, int x, int y)
+{
+	Graphics G(hdc);
+	HRSRC hResource = FindResource(g_hInst, MAKEINTRESOURCE(ID), TEXT("PNG"));
+	if (!hResource)
+		return;
+
+	DWORD imageSize = SizeofResource(g_hInst, hResource);
+	HGLOBAL hGlobal = LoadResource(g_hInst, hResource);
+	LPVOID pData = LockResource(hGlobal);
+
+	HGLOBAL hBuffer = GlobalAlloc(GMEM_MOVEABLE, imageSize);
+	LPVOID pBuffer = GlobalLock(hBuffer);
+	CopyMemory(pBuffer, pData, imageSize);
+	GlobalUnlock(hBuffer);
+
+	IStream *pStream;
+	HRESULT hr = CreateStreamOnHGlobal(hBuffer, TRUE, &pStream);
+
+	Image I(pStream);
+	pStream->Release();
+	if (I.GetLastStatus() != Ok) return;
+
+	G.DrawImage(&I, x, y, I.GetWidth(), I.GetHeight());
+}
+
+
+void OnPaintA(HDC hdc, int ID, int x, int y, double alpha)
+{
+	Graphics G(hdc);
+	HRSRC hResource = FindResource(g_hInst, MAKEINTRESOURCE(ID), TEXT("PNG"));
+	if (!hResource)
+		return;
+
+	ColorMatrix ClrMatrix =
+	{
+		1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, alpha, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	ImageAttributes ImgAttr;
+	ImgAttr.SetColorMatrix(&ClrMatrix, ColorMatrixFlagsDefault,ColorAdjustTypeBitmap);
+
+	DWORD imageSize = SizeofResource(g_hInst, hResource);
+	HGLOBAL hGlobal = LoadResource(g_hInst, hResource);
+	LPVOID pData = LockResource(hGlobal);
+
+	HGLOBAL hBuffer = GlobalAlloc(GMEM_MOVEABLE, imageSize);
+	LPVOID pBuffer = GlobalLock(hBuffer);
+	CopyMemory(pBuffer, pData, imageSize);
+	GlobalUnlock(hBuffer);
+
+	IStream *pStream;
+	HRESULT hr = CreateStreamOnHGlobal(hBuffer, TRUE, &pStream);
+
+	Image I(pStream);
+	pStream->Release();
+	if (I.GetLastStatus() != Ok) return;
+
+	RectF destination(0, 0, I.GetWidth(), I.GetHeight());
+	G.DrawImage(&I, destination, x, y, I.GetWidth(), I.GetHeight(), UnitPixel, &ImgAttr);
 }
